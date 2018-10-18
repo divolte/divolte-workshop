c5b2bf07c2126489a2cfb74bb79cb2d3aed546b2
Andrew Snare <andrewsnare@godatadriven.com>
2018-10-18 23:19:48 +0200

    Exercise 2 is completed.

--- divolte/mapping.groovy
+++ divolte/mapping.groovy
@@ -12,6 +12,37 @@ mapping {
     map eventType() onto 'eventType'
 
     // EXERCISE 2: INSERT SECTION HERE
+    section {
+        def locationUri = parse location() to uri
+        def locationPath = locationUri.path()
+        when locationPath.equalTo('/') apply {
+            map 'home' onto 'pageType'
+            // Skip rest of section.
+            exit()
+        }
+        // Regular expression for extracting pieces out of the path.
+        def pathMatcher = match '^/([a-z]+)/([^/]+)?[/]?([^/]+)?.*' against locationPath
+        when pathMatcher.matches() apply {
+            map pathMatcher.group(1) onto 'pageType'
+            when pathMatcher.group(1).equalTo('category') apply {
+                map pathMatcher.group(2) onto 'category'
+                when pathMatcher.group(3).isAbsent() apply {
+                    map 0 onto 'page'
+                }
+                map { parse pathMatcher.group(3) to int32 } onto 'page'
+                exit()
+            }
+            when pathMatcher.group(1).equalTo('product') apply {
+                map pathMatcher.group(2) onto 'productId'
+                exit()
+            }
+            when pathMatcher.group(1).equalTo('download') apply {
+                map pathMatcher.group(2) onto 'downloadId'
+                exit()
+            }
+            exit()
+        }
+    }
 
     // EXERCISE 3: INSERT SECTION HERE
 }
